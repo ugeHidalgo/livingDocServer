77be36e8150767d76169729056fe1784e8550657
Eugenio Hidalgo Hernàndez <eugenio.hidalgo@unit4.com>
2017-02-16 11:43:59 +0100

    Refactor to server.js and new batch file to regenerate doc.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
+var http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs'),
     port = process.argv[2] || 9090;
 
 http.createServer(function(request, response) {
@@ -10,37 +10,37 @@ http.createServer(function(request, response) {
     , filename = path.join(process.cwd(), uri);
 
   var contentTypesByExtension = {
-    '.html': "text/html",
-    '.css':  "text/css",
-    '.js':   "text/javascript"
+    '.html': 'text/html',
+    '.css':  'text/css',
+    '.js':   'text/javascript'
   };
 
   fs.exists(filename, function(exists) {
     if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
+      response.writeHead(404, {'Content-Type': 'text/plain'});
+      response.write('404 Not Found\n');
       response.end();
       return;
     }
 
     if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
-    fs.readFile(filename, "binary", function(err, file) {
+    fs.readFile(filename, 'binary', function(err, file) {
       if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
+        response.writeHead(500, {'Content-Type': 'text/plain'});
+        response.write(err + '\n');
         response.end();
         return;
       }
 
       var headers = {};
       var contentType = contentTypesByExtension[path.extname(filename)];
-      if (contentType) headers["Content-Type"] = contentType;
+      if (contentType) headers['Content-Type'] = contentType;
       response.writeHead(200, headers);
-      response.write(file, "binary");
+      response.write(file, 'binary');
       response.end();
     });
   });
 }).listen(parseInt(port, 10));
 
-console.log("Access server at http://localhost:" + port + "/\nUse CTRL + C to shutdown server.")
+console.log('\nServer running at http://localhost:' + port + '\n\nUse CTRL + C to shutdown server.')
